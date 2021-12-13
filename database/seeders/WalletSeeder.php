ee00bfaf910bc17c8a07129f37fd4c89a54b2253
Zen <fauz.fauz23@gmail.com>
2021-12-13 11:08:03 +0700

    aa

--- database/seeders/WalletSeeder.php
+++ database/seeders/WalletSeeder.php
@@ -20,6 +20,7 @@ class WalletSeeder extends Seeder
             'key' => Crypt::encrypt("6f8387c6e58f9c08c71869174ad2942d6b720aa6332f9cf703f1e7a70e90e084"),
             'costWd' => 1,
             'diff' => 2331,
+            'divider' => 112,
         ]);
     }
 }
